db361fd31afbfa9a19ade949dff538a8d2ba0fe7
Reuben <reubendawemarketing@gmail.com>
2025-05-19 19:16:50 +0100

    Pulling in PHP for Page title and 'generic content' div

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -9,7 +9,7 @@ while(have_posts()) {
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Our History</h1>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
           <p>Learn how the school of your dreams got started.</p>
         </div>
@@ -32,11 +32,10 @@ while(have_posts()) {
       </div>
 
       <div class="generic-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+        <?php the_content(); ?>
       </div>
     </div>
-    
+
 <?php }
 
 get_footer();
