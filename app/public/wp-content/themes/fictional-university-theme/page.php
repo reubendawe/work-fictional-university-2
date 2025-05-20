19306953463640665cc0a0817fad0da211deb481
Reuben <reubendawemarketing@gmail.com>
2025-05-20 08:10:24 +0100

    Experimenting with PHP 'if' statements

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -17,9 +17,17 @@ while(have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
+
+    <!-- Because the condition is correct, PHP will display 'the sky is blue' -->
+    <?php 
+     if (2 + 2 == 4) {
+        echo "The sky is blue";
+     }
+    ?>
+        
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
 
